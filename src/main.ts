cb8be8a5d0bc4cf86c77994d9094eaad16d4047b
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2025-08-31 21:42:05 +0200

    feat(main.ts): Provide WebApp

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import App from './App.vue'
 import { router } from './router.ts'
 import './style.css'
 
+import { provideWebApp } from './shared/telegram/web-app-plugin.ts'
+
 const app = createApp(App)
+
 app.use(createPinia())
 app.use(router)
+
+provideWebApp()
+
 app.mount('#app')
