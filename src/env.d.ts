7764e853abf25455184cded112928861264b7551
marker dao ® | Roman Ty <mark_nikolsky@outlook.com>
2025-08-31 20:41:58 +0200

    feat(.end): Add env

--- src/env.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_CG_API_URL: string
-  readonly VITE_CG_API_KEY?: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_TG_MOCK: 'true' | 'false'
+  readonly VITE_TG_THEME?: 'light' | 'dark'
+  readonly VITE_TG_USER_NAME?: string
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
